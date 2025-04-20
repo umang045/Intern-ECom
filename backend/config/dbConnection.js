d5727ca849712366a769cec7dfa1c782b922eb3f
umangwsc <umang.rathod@weservecodes.com>
2025-04-20 18:46:36 +0530

    final push railway 6.0

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,19 +2,24 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config(); // Load environment variables
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST || "switchback.proxy.rlwy.net", // External hostname
-  port: process.env.MYSQLPORT || 38906, // External port
-  user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "xGbhWxAjkWExvjLURYyGYAtErRtaVdOH",
-  database: process.env.MYSQLDATABASE || "railway",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  // Enable SSL for Railway (optional, but recommended)
-  ssl: {
-    rejectUnauthorized: true, // Enforce SSL verification
-  },
-});
+// const db = mysql.createPool({
+//   host: process.env.MYSQLHOST || "switchback.proxy.rlwy.net", // External hostname
+//   port: process.env.MYSQLPORT || 38906, // External port
+//   user: process.env.MYSQLUSER || "root",
+//   password: process.env.MYSQLPASSWORD || "xGbhWxAjkWExvjLURYyGYAtErRtaVdOH",
+//   database: process.env.MYSQLDATABASE || "railway",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   // Enable SSL for Railway (optional, but recommended)
+//   ssl: {
+//     rejectUnauthorized: true, // Enforce SSL verification
+//   },
+// });
+
+const db = async () => {
+  const connection = await mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
+  return connection;
+};
 
 module.exports = { db };
